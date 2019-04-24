30455b57a87a6d1cafc0b1314e81caf386a5f0de
Paul Thompson <paul.j.m.thompson@outlook.com>
2019-04-24 19:35:00 +0100

    Version change to v2.0.0 (beta)

--- src/version.h
+++ src/version.h
@@ -2,14 +2,14 @@
 #define STR(x) STR_HELPER(x)
 
 #define PROJECT_NAME "Worktips"
-#define PROJECT_SITE "http://www.worktips.info"
-#define PROJECT_COPYRIGHT "Copyright 2018, The Worktips Developers"
-#define APP_VER_MAJOR 1
+#define PROJECT_SITE "https://worktips.xyz/"
+#define PROJECT_COPYRIGHT "Copyright © Worktips 2019"
+#define APP_VER_MAJOR 2
 #define APP_VER_MINOR 0
-#define APP_VER_REV 2
-#define APP_VER_BUILD 3
+#define APP_VER_REV 0
+#define APP_VER_BUILD 0
 
-#define BUILD_COMMIT_ID "4.0.0"
+#define BUILD_COMMIT_ID "2.0.0-beta"
 #define PROJECT_VERSION STR(APP_VER_MAJOR) "." STR(APP_VER_MINOR) "." STR(APP_VER_REV)
 #define PROJECT_VERSION_BUILD_NO STR(APP_VER_BUILD)
 #define PROJECT_VERSION_LONG PROJECT_VERSION "." PROJECT_VERSION_BUILD_NO " "
